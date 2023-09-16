17205c2521bb0a6b5baf198c13bdaa7dc61fcb9d
FedeeHerrera <fedeeherrera2001@gmail.com>
2023-09-15 23:11:28 -0300

    Creacion del proyecto y fetch endpoint

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Personas from './components/Personas'
+
+function App() {
+  return (
+    <>
+      <h1>Hola Mundo Desde React</h1>
+      <Personas />
+    </>
+  )
+}
+
+export default App
