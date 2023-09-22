0955fe4fb405da583d4af3968c7239bc0da97a50
FedeeHerrera <fedeeherrera2001@gmail.com>
2023-09-22 15:54:15 -0300

    Cree formulario donacion, estile el formulario y refactorice Codigo

--- src/Donaciones/Form.jsx
+++ src/Donaciones/Form.jsx
@@ -1,7 +1,19 @@
-
 function Form() {
   return (
-    <div>Form</div>
+    <form className="form">
+      <div className="div_nombre_donacion">
+        <label htmlFor="Name">Nombre y Apellido</label>
+        <input className="input_name" type="text" id="Name" name="Name" />
+        <label htmlFor="Donacion">Donacion</label>
+        <input className="input_donacion" type="text" id="Donacion" name="Donacion" />
+      </div>
+      <div className="div_mensaje">
+        <label htmlFor="Mensaje">Mensaje</label>
+        <input className="input_mensaje" type="text" id="Mensaje" name="Mensaje" />
+      </div>
+      <br />
+      <button className="submit">Enviar Donacion</button>
+    </form>
   )
 }
 
