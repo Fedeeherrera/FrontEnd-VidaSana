5f6a07e736a3db19e8261c7ef7bcced2fba16859
EmilianoSanchezz <sanchez.emilianou@gmail.com>
2023-10-05 19:29:57 -0300

    header y footer

---
+++ src/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import MiHeader from "../components/Header";
+import MiFooter from "../components/Footer";
+
+function Home () {
+    return (
+          <div>
+            <MiHeader/>
+              <h1>Home</h1>
+            <MiFooter/>
+          </div>
+    )
+  }
+  
+  export default Home
+  
--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import MiHeader from "../components/Header";
 function Login () {
   return (
         <div>
