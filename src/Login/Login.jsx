6816734083e38d9b4cf96ac2dbe7e4522acb53fe
EmilianoSanchezz <sanchez.emilianou@gmail.com>
2023-11-20 18:36:14 -0300

    header footer y login terminado

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,10 +1,56 @@
 import React from "react";
-import MiHeader from "../components/Header";
-function Login () {
+import './Login.css'
+import { useState } from 'react';
+
+function Login ({setUser}) {
+  const [username, setUsername] = useState("")
+  const [correo, setCorreo] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(false)
+
+
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+      if(username === "" || correo === "" || password === "" ){
+        setError(true)
+        return 
+      }
+
+      setError(false)
+      setUser([username, correo, password])
+  }
+
   return (
-        <div>
-            <h1>Login</h1>
+<div className="container">
+        <form onSubmit={handleSubmit}>
+
+      
+        <div className="form">
+        <h2>Login Form</h2>
+          <div className="campo">
+            <label>Username</label>
+            <input type="text" name="username" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+          </div>
+          <div className="campo">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Email" value={correo} onChange={e => setCorreo(e.target.value)}/>
+          </div>
+          <div className="campo">
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+          </div>
+          <button className="button"  >Submit</button>
+        {error && <p>Todos los campos son obligatorios</p>}
         </div>
+        <p className="text"><small>&copy; 2023 <b>Vida Natural</b></small></p>
+        </form>
+      </div>
+          
+
+
+     
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,25 @@ import './Header.css'
 import React from 'react'
 function Header () {
   return (
-    <div className="header">
-      <div className="container">
-        <a href="" className="logo"><img src="" alt="" />Vida Natural</a>
-      </div>
-
-      <div className="navegacion">
-       <ul>
-          <li><a href="http://localhost:5173/Home">Home</a></li>
-          <li><a href="http://localhost:5173/Contaminacion">Contaminacion</a></li>
-          <li><a href="http://localhost:5173/Donaciones">Donaciones</a></li>
-          <li><a href="http://localhost:5173/Login">Login</a></li>
-          
-        </ul>
-      </div>
+  <>
+  <div className="back">
+    <div className="menu-contenedor">
+    <a href="#" className='logo'><img src="./src/components/img/logoPropio.png" alt="LogoVidaNatural" /></a>
+    <div className="menuNav">
+      <ul>
+      <li><a href="http://localhost:5173/Home">Home</a></li>
+      <li><a href="http://localhost:5173/Contaminacion">Contaminacion</a></li>
+      <li><a href="http://localhost:5173/Donaciones">Donaciones</a></li>
+      <li><a href="http://localhost:5173/Login">Login</a></li>
+      </ul>
     </div>
+
+    </div>
+  </div>
+  </>
 
   )
 }
 export default Header
+
+
