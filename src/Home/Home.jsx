273a2ce484ca4b152635ff8a5f8039ae1a1a84d0
Federico Herrera Modernizacion <fedeeherrera2001@gmail.com>
2024-05-31 13:15:43 -0300

    refactorizacion de codigo

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,37 +1,31 @@
 import React from 'react'
 import './Home.css'
-import LogrosContenidos from './Logros.jsx'
-
 
 function Home () {
   return (
     <div className="home">
-
-  <div className="inicio">
-
-      <div className="inicioContenido">
-      <h1>ONG Vida Natural </h1>
-      <p>Nuestra ONG "Vida Natural" es una entidad civil fundada el 29 de junio de 2010, que trabaja en la solución de los principales problemas ambientales de Argentina. Nuestra misión es "proponer e implementar soluciones para conservar la naturaleza, promover el uso sustentable de los recursos naturales y una conducta responsable del hombre en un contexto de cambio climático". Para esto, desarrollamos acciones basadas en la protección de áreas naturales, el consumo y la producción responsable, la legislación ambiental y la concientización y educación ambiental. Desde 2011 es la entidad representante para Argentina de la Organización Mundial de Conservación (WWF), la organización ambiental más grande del mundo. La visión de Vida Natural es "un mundo en el cual el ser humano se desarrolle en armonía con la naturaleza". En la actualidad, su trabajo se enfoca en cinco ecorregiones prioritarias de la Argentina por su rica biodiversidad que se encuentra amenazada: la selva paranaense, el Gran Chaco, las pampas, mares y costas (la Antártida y océanos australes), ríos y lagos del sur. Vida Natural posee tres oficinas, ubicadas en la Ciudad Autónoma de Buenos Aires, Puerto Iguazú y Lago Puelo. Además, cuenta con dos reservas propias: Urugua-í en Misiones, y San Pablo de Valdés, en Chubut.
-        </p>
-        
-      <a href="http://localhost:5173/Donaciones" className='btn'>Realizar Donacion</a> 
+      <div className="inicio">
+        <div className="inicioContenido">
+          <h1>ONG Vida Natural </h1>
+          <p>Nuestra ONG "Vida Natural" es una entidad civil fundada el 29 de junio de 2010, que trabaja en la solución de los principales problemas ambientales de Argentina. Nuestra misión es "proponer e implementar soluciones para conservar la naturaleza, promover el uso sustentable de los recursos naturales y una conducta responsable del hombre en un contexto de cambio climático". Para esto, desarrollamos acciones basadas en la protección de áreas naturales, el consumo y la producción responsable, la legislación ambiental y la concientización y educación ambiental. Desde 2011 es la entidad representante para Argentina de la Organización Mundial de Conservación (WWF), la organización ambiental más grande del mundo. La visión de Vida Natural es "un mundo en el cual el ser humano se desarrolle en armonía con la naturaleza". En la actualidad, su trabajo se enfoca en cinco ecorregiones prioritarias de la Argentina por su rica biodiversidad que se encuentra amenazada: la selva paranaense, el Gran Chaco, las pampas, mares y costas (la Antártida y océanos australes), ríos y lagos del sur. Vida Natural posee tres oficinas, ubicadas en la Ciudad Autónoma de Buenos Aires, Puerto Iguazú y Lago Puelo. Además, cuenta con dos reservas propias: Urugua-í en Misiones, y San Pablo de Valdés, en Chubut.
+          </p>
+          <a href="http://localhost:5173/Donaciones" className='btn'>Realizar Donacion</a>
+        </div>
       </div>
-  </div>
-    <div className="tarjetaInfo">
+      <div className="tarjetaInfo">
         <div className="tarjeta-Img">
-          <img src="/src/Home/Img/reforestacion.jpg" alt=""/>
+          <img src="/src/Home/Img/reforestacion.jpg" alt="" />
         </div>
         <div className="tarjeta-text">
           <h2>MAS ARBOLES, MAS OXIGENO</h2>
           <p>Logramos la reforestacion Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fugit est laborum eum consequatur veritatis sequi esse, ipsum exercitationem sapiente repellendus illo consectetur excepturi voluptates iusto atque voluptas delectus porro?</p>
         </div>
-    </div>
-
-    <div className="tour">
-      <div className="center-text">
-        <h2>MAS INFORMACION</h2>
       </div>
-      <div className="localidad-content">
+      <div className="tour">
+        <div className="center-text">
+          <h2>MAS INFORMACION</h2>
+        </div>
+        <div className="localidad-content">
           <div className="box">
             <img src="/src/Home/Img/reforestacion1.jpg" alt="" />
             <h6>Argentina</h6>
@@ -47,25 +41,23 @@ function Home () {
             <h6>Cordoba</h6>
             <h4>Empresas de Arboles</h4>
           </div>
-      </div>
+        </div>
         <div className="center-btn"><a href="http://localhost:5173/Contaminacion" className='btn'>Leer Mas</a></div>
-    </div>
-
-    <div className="tarjetaInfo">
+      </div>
+      <div className="tarjetaInfo">
         <div className="tarjeta-text">
           <h2>PLAYAS LIMPIAS</h2>
-          <p>Junto a Voluntarios logramos limpiar km de playas Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, voluptatibus dolor doloremque commodi natus esse suscipit nesciunt quas eveniet accusantium harum mollitia obcaecati ut ipsam placeat itaque nisi aspernatur repudiandae!</p> 
+          <p>Junto a Voluntarios logramos limpiar km de playas Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, voluptatibus dolor doloremque commodi natus esse suscipit nesciunt quas eveniet accusantium harum mollitia obcaecati ut ipsam placeat itaque nisi aspernatur repudiandae!</p>
         </div>
         <div className="tarjeta-Img">
-          <img src="/src/Home/Img/limpieza3.jpeg" alt=""/>
+          <img src="/src/Home/Img/limpieza3.jpeg" alt="" />
         </div>
-    </div>
-   
-    <div className="tour">
-      <div className="center-text">
-        <h2>MAS INFORMACION</h2>
       </div>
-      <div className="localidad-content">
+      <div className="tour">
+        <div className="center-text">
+          <h2>MAS INFORMACION</h2>
+        </div>
+        <div className="localidad-content">
           <div className="box">
             <img src="/src/Home/Img/playalimpia1.jpg" alt="" />
             <h6>Argentina</h6>
@@ -81,84 +73,10 @@ function Home () {
             <h6>Villa Gesel</h6>
             <h4>Camiones de Residuos</h4>
           </div>
-      </div>
+        </div>
         <div className="center-btn"><a href="http://localhost:5173/Contaminacion" className='btn'>Leer Mas</a></div>
-    </div> 
-
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-    /*<div className="inicio">
-      
-      <div className="fila">
-        <div className="columna">
-          <h2>VIDA NATURAL</h2>
-          <h5>organización no gubernamental</h5>
-        </div>
-        
-        <p> Nuestra ONG "Vida Natural" es una entidad civil fundada el 29 de junio de 2010, que trabaja en la solución de los principales problemas ambientales de Argentina. Nuestra misión es "proponer e implementar soluciones para conservar la naturaleza, promover el uso sustentable de los recursos naturales y una conducta responsable del hombre en un contexto de cambio climático". Para esto, desarrollamos acciones basadas en la protección de áreas naturales, el consumo y la producción responsable, la legislación ambiental y la concientización y educación ambiental. Desde 2011 es la entidad representante para Argentina de la Organización Mundial de Conservación (WWF), la organización ambiental más grande del mundo. La visión de Vida Natural es "un mundo en el cual el ser humano se desarrolle en armonía con la naturaleza". En la actualidad, su trabajo se enfoca en cinco ecorregiones prioritarias de la Argentina por su rica biodiversidad que se encuentra amenazada: la selva paranaense, el Gran Chaco, las pampas, mares y costas (la Antártida y océanos australes), ríos y lagos del sur. Vida Natural posee tres oficinas, ubicadas en la Ciudad Autónoma de Buenos Aires, Puerto Iguazú y Lago Puelo. Además, cuenta con dos reservas propias: Urugua-í en Misiones, y San Pablo de Valdés, en Chubut.
-        </p>
       </div>
-      
-      <div className="columna">
-        <div className='imgContenedor'>
-        <img src="./src/components/img/logoPropio.png" alt="" />
-        </div>
-      </div>
-
-
-      <div className="columna">
-        <div className='imgContenedor'>
-          <LogrosContenidos
-            imageUrl='https://wallpapercave.com/wp/wp7532100.jpg'
-            title="Más árboles, más oxigeno"
-            description="Logramos reforestar con más de 2000 nuevos árboles"
-          />
-        </div>
-        <div className='card-contenido'>
-          <LogrosContenidos
-            imageUrl='https://www.vidasostenible.org/wp-content/uploads/2019/11/PlayasLimpias2.jpg'
-            title="Playas Limpias"
-            description="Junto a Voluntarios logramos limpiar km de playas"
-          />
-        </div>
-        <div className='card-contenido'>
-          <LogrosContenidos
-            imageUrl='https://www.junin.gob.ar/sites/default/files/noticias/charla_medio_ambiente_en_colegio_san_jose_2.jpg'
-            title="Cuidemos el Agua"
-            description="Campaña de Concientización: Charlas en escuelas "
-          />
-        </div>
-        <div className='card-contenido'>
-          <LogrosContenidos
-            imageUrl='https://www.ecologistasenaccion.org/wp-content/uploads/2022/05/activistas-brasil-denuncias-deforestacion.jpg'
-            title="Desforestacion"
-            description="Protestas ante empresas responsables de la deforestación"
-          />
-        </div>
-        <div className='card-contenido'>
-          <LogrosContenidos
-            imageUrl='https://www.mundomarino.com.ar/alpza/images/secciones/fmm12.jpg'
-            title="Cuidando Nuestros Mares"
-            description="Junto a Voluntarios logramos salvar a animales marinos lastimados, siendo devueltos a su habitat natural"
-          />
-        </div>
-      </div>
-        
     </div>
-    
-    */
   )
 };
-export default Home 
+export default Home
