098010382b30180aabda12d760120f4fb68900d0
Fedeeherrera <fedeeherrera2001@gmail.com>
2024-07-01 12:54:49 -0300

    Cambios en el Login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,42 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
-function Login () {
+
+import swal from 'sweetalert'
+
+function Login ({ setUser }) {
+  const [username, setUsername] = useState('')
+  const [correo, setCorreo] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(false)
+
+  const mostrarAlert = () => {
+    if (username === '' || correo === '' || password === '') {
+      setError(true)
+      swal({
+        title: 'Error',
+        text: 'Todos los campos deben esta completos',
+        icon: 'error',
+        button: 'Aceptar'
+      })
+      return
+    }
+
+    setError(false)
+    swal({
+      title: 'Completado',
+      text: 'Los datos se ingrasaron con exito',
+      icon: 'success',
+      button: 'Aceptar'
+    })
+
+    setUser([username, correo, password])
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (username === '' || correo === '' || password === '') {
+      setError(true)
+      return
+    }
+
+    setError(false)
+    setUser([username, correo, password])
+  }
+
   return (
-    <div className="login">
+<div className="container">
+        <form onSubmit={handleSubmit}>
 
-        <div className="containerLogin">
-          <div className="tituloForm">
-              <div className="text">Sign Up</div>
-              <div className="underline"></div>
+        <div className="form">
+        <h2>Login Form</h2>
+          <div className="campo">
+            <label>Username</label>
+            <input type="text" name="username" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
           </div>
-
-            <div className="inputs">
-              <div className="input">
-                <img src="./src/Login/user.png" alt="" className="imgLogin"/>
-                <input type="text" placeholder="Nombre de Usuario"/>
-              </div>
-            </div>
-
-            <div className="inputs">
-              <div className="input">
-              <img src="./src/Login/email.png" alt="" className="imgLogin" />
-
-                <input type="email" placeholder="Ingrese Email"/>
-              </div>
-            </div>
-
-            <div className="inputs">
-              <div className="input">
-              <img src="./src/Login/password.png" alt="" className="imgLogin"/>
-                <input type="password" placeholder="Ingrese Contraseña" />
-              </div>
-            </div>
-            <div className="submit-container">
-              <div className="submit-btn">Sign Up</div>
-              <div className="submit-btn">Login</div>
-            </div>
+          <div className="campo">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Email" value={correo} onChange={e => setCorreo(e.target.value)}/>
+          </div>
+          <div className="campo">
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+          </div>
+          <button className="button" onClick={() => mostrarAlert()} >Submit</button>
+        {error && <p>Todos los campos son obligatorios</p>}
         </div>
-    </div>
+        <p className="text"><small>&copy; 2023 <b>Vida Natural</b></small></p>
+        </form>
+      </div>
+
   )
 }
 
